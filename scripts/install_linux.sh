8d719852114f57fc3579e72f46b066f1685c1fc9
containerscrew <131241415+containerscrew@users.noreply.github.com>
2023-05-22 23:01:49 +0200

    Fixing linus script installation

--- scripts/install_linux.sh
+++ scripts/install_linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-TFTOOLS_CLI_VERSION=latest
+TFTOOLS_LATEST_VERSION=$(curl -s https://api.github.com/repos/containerscrew/tftools/releases/latest | jq -r ".name")
 TFTOOLS_CLI_ARCH=amd64
-if [ "$(uname -m)" = "aarch64" ]; then CLI_ARCH=arm64; fi
-curl -L --fail --remote-name-all https://github.com/containerscrew/tftools/releases/download/${TFTOOLS_CLI_VERSION}/tftools-linux-${TFTOOLS_CLI_ARCH}.tar.gz{,.sha256sum}
-sha256sum --check tftools-${CLI_ARCH}.tar.gz.sha256sum
+if [ "$(uname -m)" = "aarch64" ]; then TFTOOLS_CLI_ARCH=arm64; fi
+curl -L --fail --remote-name-all https://github.com/containerscrew/tftools/releases/download/${TFTOOLS_LATEST_VERSION}/tftools-linux-${TFTOOLS_CLI_ARCH}.tar.gz{,.sha256sum}
+sha256sum --check tftools-linux-${TFTOOLS_CLI_ARCH}.tar.gz.sha256sum
 sudo tar xzvfC tftools-${CLI_ARCH}.tar.gz /usr/local/bin
 rm tftools-${CLI_ARCH}.tar.gz{,.sha256sum}
